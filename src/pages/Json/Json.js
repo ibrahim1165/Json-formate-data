bd66612f7dbd989cc1f7df2156ceca643c39c65c
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-21 18:19:10 +0600

    json formet and clear json added

--- src/pages/Json/Json.js
+++ src/pages/Json/Json.js
@@ -2,31 +2,30 @@ import React from 'react';
 import "./json.css"
 
 const Json = () => {
-    const inputArea = document.querySelector(".large-area--input");
+    const inputArea = document?.querySelector(".large-area--input");
     const outputArea = document.querySelector(".large-area--output");
-    const btnFormat = document.querySelector(".controls__button--format");
-    // const btnMinify = document.querySelector(".controls__button--minify");
-    
-    // btnFormat?.addEventListener("click", () => {
-      
-    // });
-    const handleClick =()=>{
+
+    const handleClick = () => {
         const formatted = JSON.stringify(JSON.parse(inputArea.value), null, 4);
         outputArea.value = formatted;
     }
-
+    const handleClear = () => {
+        outputArea.value="";
+        inputArea.value="";
+    }
 
 
     return (
         <div class="container">
             <textarea class="large-area large-area--input" placeholder="Enter your JSON here...">
-              
+
             </textarea>
             <div class="controls">
                 <button onClick={handleClick} type="button" class="controls__button controls__button--format">Format</button>
-                <button type="button" class="controls__button controls__button--minify">Minify</button>
+
+                <button onClick={handleClear} type="button" class="controls__button controls__button--minify">Clear</button>
             </div>
-            <textarea  class="large-area large-area--output" placeholder="Your JSON will appear here..." readOnly></textarea>
+            <textarea class="large-area large-area--output" placeholder="Your JSON will appear here..." readOnly></textarea>
         </div>
     )
 }
